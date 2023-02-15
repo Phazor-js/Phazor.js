ae8ee17692c23bc70c5232705f48b9a5559f7468
plurryt <70047632+pythoniaweb@users.noreply.github.com>
2023-02-15 19:10:43 +0000

    modified:   language.js

--- language.js
+++ language.js
@@ -1,2 +1,6 @@
+// EN / US - English
 // Hello this file created for github languages config
 // U can see github languages by this file 
+// UA - Українська
+// Привіт ти можеш бачити список ЯП в github завдяки цьому файлу
+// Будь ласка нічого тут не міняй
